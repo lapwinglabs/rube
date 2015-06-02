3dc4a3245257f2d1f1c944f868c832d4ad7f8803
Matthew Mueller <mattmuelle@gmail.com>
2015-06-02 00:33:56 -0700

    rework

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 
 var error = require('./lib/utils/error');
 var type = require('./lib/utils/type');
+var clone = require('component-clone');
 var bind = Function.prototype.bind;
 var sliced = require('sliced');
-var Step = require('step.js');
-var Batch = require('batch');
 var noop = function() {};
+var Vo = require('vo');
 
 /**
  * Export `Rube`
@@ -37,51 +37,92 @@ var types = {
  */
 
 function Rube() {
-  if (!(this instanceof Rube)) return construct(Rube, sliced(arguments));
   var args = sliced(arguments);
-  if (args.length <= 1) return initialize(args[0]);
+  var pipeline = args.map(seed);
 
-  args = args.map(initialize);
-  var len = args.length;
-
-  return function rube(actual, fn) {
-    var batch = Batch().throws(false);
-
-    args.forEach(function(arg) {
-      batch.push(function(next) {
-        arg(actual, next);
-      });
-    });
-
-    batch.end(function(errors, values) {
-      errors = errors.filter(function(err) { return err; });
-      return errors.length == len
-        ? fn(error(errors, actual))
-        : fn(null, actual);
-    });
-  }
-
-  function initialize(value) {
-    // instanceof itself
-    // TODO: improve?
-    if (value.rube) return value;
-
-    switch (type(value)) {
-      case 'string': return types.string(value);
-      case 'number': return types.number(value);
-      case 'object': return types.object(value);
-      case 'array': return types.array(value);
-      default: return types.any(value);
-    }
+  if (1 == arguments.length) {
+    return pipeline[0];
   }
 }
 
-/**
- * Construct
- */
+function seed(value) {
+  value = clone(value);
 
-function construct(constructor, args) {
-  function F() { return constructor.apply(this, args); }
-  F.prototype = constructor.prototype;
-  return new F();
+  switch (type(value)) {
+    case 'string': return types.string(value);
+    case 'number': return types.number(value);
+    case 'object': return types.object(value);
+    case 'array': return types.array(value);
+    default: return types.any(value);
+  }
 }
+
+var rube = Rube({
+  a: Rube(String),
+  b: Rube(String),
+  c: Rube(String)
+}).or('a', 'b').only('c');
+
+rube.only('c')('hi there!', function(err, v) {
+  if (err) throw err;
+  console.log(v);
+})
+
+// var a = Rube(String).between(4, 5);
+// var b = Rube(Number).cast(String, Number).between(10, 20);
+
+// b('15', function(err, v) {
+//   if (err) throw err;
+//   console.log(v);
+// })
+
+
+// function Rube() {
+//   if (!(this instanceof Rube)) return construct(Rube, sliced(arguments));
+//   var args = sliced(arguments);
+//   if (args.length <= 1) return initialize(args[0]);
+
+//   args = args.map(initialize);
+//   var len = args.length;
+
+//   return function rube(actual, fn) {
+//     var batch = Batch().throws(false);
+
+//     args.forEach(function(arg) {
+//       batch.push(function(next) {
+//         arg(actual, next);
+//       });
+//     });
+
+//     batch.end(function(errors, values) {
+//       errors = errors.filter(function(err) { return err; });
+//       return errors.length == len
+//         ? fn(error(errors, actual))
+//         : fn(null, actual);
+//     });
+//   }
+
+//   function initialize(value) {
+//     // instanceof itself
+//     // TODO: improve?
+//     if (value.rube) return value;
+
+//     switch (type(value)) {
+//       case 'string': return types.string(value);
+//       case 'number': return types.number(value);
+//       case 'object': return types.object(value);
+//       case 'array': return types.array(value);
+//       default: return types.any(value);
+//     }
+//   }
+// }
+
+// /**
+//  * Construct
+//  */
+
+// function construct(constructor, args) {
+//   function F() { return constructor.apply(this, args); }
+//   F.prototype = constructor.prototype;
+//   return new F();
+// }
